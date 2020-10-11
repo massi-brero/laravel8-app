0545c2d3babe309fd318fcb88e72d53336f8e977
massi.brero@gmail.com <massi.brero@gmail.com>
2020-10-11 15:52:44 +0200

    first commit

---
+++ resources/views/start.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'MyHobbies')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Startseite</div>
+
+                <div class="card-body">
+                    Willkommen bei Massis MyHobbies!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
